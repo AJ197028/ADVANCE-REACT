e1d474d8ee2e7829cfe0e485fc5f1556f87edfcc
AJ197028 <aksjais20@gmail.com>
2023-01-11 14:36:04 +0530

    handling user unput

--- Catching User Input/src/components/NewExpense/ExpenseForm.js
+++ Catching User Input/src/components/NewExpense/ExpenseForm.js
@@ -9,11 +9,11 @@ const ExpenseForm = () =>{
         <form>
             <div className='new-expense__controls'>
                 <div className='new-expense__control'>
-                    <label>Title</label>
+                    <label>Expense Title</label>
                     <input type='text' onChange={titleChangeHandler}/>
                 </div>
                 <div className='new-expense__control'>
-                    <label>Amount</label>
+                    <label>Expense Amount</label>
                     <input type='number' min='0.01' step='0.01' />
                 </div>
                 <div className='new-expense__control'>
